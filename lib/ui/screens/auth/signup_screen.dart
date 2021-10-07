6c764d7b05df2f234e6c251bd149aa597d241f5d
CharukaWijethunga <charukamwijethunga@gmail.com>
2021-10-07 20:04:02 +0530

    remove the hardcoded values in pending and completed tasks

--- lib/ui/screens/auth/signup_screen.dart
+++ lib/ui/screens/auth/signup_screen.dart
@@ -90,7 +90,7 @@ class _SignupScreenState extends State<SignupScreen> {
                           name: nameController.text,
                           phoneNumber: phoneNumberController.text,
                           address: addressController.text,
-                          status: 'training_pending', uid: '', pendingTasks: ["btofkz2RBAbdRxlCXHoX"], completedTasks: ["igfAyCtmedoaE8QLFink"]));
+                          status: 'training_pending', uid: '', pendingTasks: [], completedTasks: []));
 
                 },
                 child: Ink(
