009739383ff9b8d820a792c16b6427afe3f808e5
Diksha Gohlyan <gr8bigdg@gmail.com>
2021-11-09 14:10:46 -0800

    Refresh task once task is uploaded

--- lib/repositories/task_repository.dart
+++ lib/repositories/task_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/all.dart';
 import 'package:junkiri/models/task.dart';
 import 'package:junkiri/services/firestore_service.dart';
@@ -14,3 +15,42 @@ final newTaskProvider = FutureProvider.autoDispose<List<Task>>((ref) async {
   List<Task> taskList = await _firestoreService.getListofNewTasks();
   return taskList;
 });
+
+class TaskNotifier extends ChangeNotifier {
+  final ProviderReference ref;
+
+  TaskNotifier(this.ref);
+  Task? _task;
+
+  Task? get task => _task;
+
+
+  void setTaskId(String taskId) {
+    FirestoreService _firestoreService = FirestoreService();
+    _firestoreService.getTask(taskId).then((value) {
+      print(task);
+      print('dikshag setting tasks' + task.toString());
+
+      _task = value;
+      notifyListeners();
+    });
+  }
+
+  void incrementSteps(Task task) {
+    print('dikshag incrementing');
+
+    FirestoreService _firestoreService = FirestoreService();
+    _firestoreService.completedTheStep(task).then((value) =>
+        _firestoreService.getTask(task.docId).then((value) {
+          print(task);
+          _task = value;
+          print('dikshag notifying listeners' + task.currentStep.toString());
+          notifyListeners();
+        }
+    ));
+  }
+}
+
+
+final currentTaskProvider =
+ChangeNotifierProvider<TaskNotifier>((ref) => TaskNotifier(ref));
